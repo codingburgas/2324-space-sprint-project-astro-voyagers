d6b2327fd681e11a4968f3beb24a184860614645
LGGospodinov <LGGospodinov22@codingburgas.bg>
2024-02-14 13:25:06 +0200

    Add cursor image and new background

--- 2324-space-sprint-project-astro-voyagers/2324-space-sprint-project-astro-voyagers.cpp
+++ 2324-space-sprint-project-astro-voyagers/2324-space-sprint-project-astro-voyagers.cpp
@@ -7,9 +7,24 @@ int main() {
 
     sf::Sprite sprite;
     sf::Texture texture;
+    sf::Texture cursorTexture;
+
+    if (!cursorTexture.loadFromFile("RocketCursor.png")) {
+        return EXIT_FAILURE;
+    }
+
+    // Create a sprite for the cursor
+    sf::Sprite cursor(cursorTexture);
+
+    // Adjust the size of the cursor image
+    float scaleFactor = 0.3f; // Change this value to adjust the size
+    cursor.setScale(scaleFactor, scaleFactor);
+
+    // Hide the default mouse cursor
+    window.setMouseCursorVisible(false);
 
     // Load the texture from the file
-    if (!texture.loadFromFile("stars.jpg")) {
+    if (!texture.loadFromFile("Space.jpg")) {
         std::cerr << "Failed to load texture." << std::endl;
         return 1; // Return an error code
     }
@@ -31,8 +46,16 @@ int main() {
             }
         }
 
-        window.clear();
+        cursor.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
+
+        window.clear(sf::Color::White);
+
+        // Draw your game objects here
         window.draw(sprite);
+
+        // Draw the custom cursor
+        window.draw(cursor);
+
         window.display();
     }
 
